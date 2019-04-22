2bb93264b7fb16c2048c6396fa5041951da22af1
Vlad Ruchko <necessarydp@gmail.com>
2019-04-22 13:13:58 +0300

    fix issue #9

--- src/js.js
+++ src/js.js
@@ -1,21 +1,28 @@
-  if (window.matchMedia("(max-width: 1000px)").matches) {
-    $(document).ready(function () {
-      $('.feedback_one').slick({
-        slidesToShow: 1,
-        dots: true,
-        prevArrow: '<div class="button_left"></div>',
-        nextArrow: '<div class="button_right"></div>',
-        dotsClass: 'dotted'
-      });
-    });
-  } else {
-    $(document).ready(function () {
-      $('.feedback_one').slick({
-        slidesToShow: 3,
-        dots: true,
-        prevArrow: '<div class="button_left"></div>',
-        nextArrow: '<div class="button_right"></div>',
-        dotsClass: 'dotted'
-      });
-    });
-  }
+ $('.feedback_one').slick({
+    slidesToShow: 3,
+    dots: true,
+    prevArrow: '<div class="button_left"></div>',
+    nextArrow: '<div class="button_right"></div>',
+    dotsClass: 'dotted',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
